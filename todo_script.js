8d4290d8abe0aacfedd08c8d78e0770283c40cb1
Bbiswabasu <58428413+Bbiswabasu@users.noreply.github.com>
2020-12-07 09:00:50 +0530

    Boilerplate code for todo list added

---
+++ todo_script.js
@@ -0,0 +1,13 @@
+const todo_list=document.getElementById('todo_list');
+
+function addTask() {  // This function is called when Add button in todo list is clicked
+    
+    //Create a container div to contain controls for i-th task
+    //The controls will be created dynamically here
+
+    //Add checkbox to conatiner and add relevant event listener
+
+    //To that container, add label showing name of task
+    
+    //Create a delete button and relevant event listener
+}
